61366faed6adc7d7a18ed46d4ffac208720b5f91
Sean Poynter-Smith <spoyntersmith@users.noreply.github.com>
2020-04-22 10:34:05 +0100

    Remove server side form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h1>Contact</h1>
 
-    <form action="https://jumprock.co/mail/{{ $page->services->jumprock }}" method="post">
+    <form name="contact" method="post" data-netlify="true">
         <div>
             <label for="name">Name</label><br>
             <input type="text" name="name" id="name" required>
@@ -21,14 +21,6 @@
             <textarea name="message" id="message" required></textarea>
         </div>
 
-        <input name="subject" style="display: none;" value="Blog Contact Page">
-
-        <input type="hidden" name="after" value="{{ $page->production ? $page->baseUrl : 'http://localhost:3000' }}/contact/sent">
-
-        <input type="text" name="trapit" value="" style="display: none;">
-
-        <input type="hidden" name="replyto" value="%email">
-
-        <input type="submit" value="Send">
+        <button type="submit" value="Send">
     </form>
 @endsection
